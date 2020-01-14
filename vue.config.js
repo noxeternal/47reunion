b4378c44405dc994efa7a63c719b1e8770bd8523
Michael Rice <michael@imsenterprises.com>
2020-01-14 10:30:46 -0600

    daily commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: 'registration'
+  publicPath: 'registration',
+  devServer: {
+    disableHostCheck: true,
+    host: 'dev.vm',
+    port: 3000,
+    proxy: {
+      '/api': 'http://dev.vm/api'
+    }
+  }
 };
