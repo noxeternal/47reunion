b9764fe5bcad31bac20c35db18254a2e8b6a814a
Adam Shumann <ashumann13@gmail.com>
2020-01-17 13:10:56 -0600

    Fix api plugin

--- src/main.js
+++ src/main.js
@@ -4,14 +4,13 @@ import App from "./App.vue";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import cookies from "./plugins/cookies";
-import api from "./plugins/api";
+import "./plugins/api";
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
   vuetify,
-  api,
   cookies,
   render: h => h(App)
 }).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,15 @@ module.exports = {
   publicPath: 'registration',
   devServer: {
     disableHostCheck: true,
-    host: 'dev.vm',
+    host: '0.0.0.0',
     port: 3000,
     proxy: {
-      '^api': {
-        target: 'http://dev.vm:3001/?',
-        changeOrigin: true
+      '^/api': {
+        target: process.env.API_URL || 'http://localhost:3001',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '' // Strip /api from path
+        }
       }
     }
   }
